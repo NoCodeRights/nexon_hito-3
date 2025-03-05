4ede0290416e51de95f088d96522804fc3d83446
Marcelo Sepulveda <marcelo.sepulveda.ramirez@gmail.com>
2025-03-05 20:11:15 -0300

    fixing db connections v2

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ const dotenv = require("dotenv");
 const path = require("path");
 const products = require("./routes/products");
 const users = require("./routes/users");
+const cart = require("./routes/cart");
+const favorites = require("./routes/favorites");
 
 dotenv.config();
 const app = express();
 
 // Middleware CORS mejorado
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://nexon-hito-3.onrender.com"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true // Permite envío de cookies/autenticación
@@ -26,6 +28,8 @@ app.use(morgan("dev")); // Para ver logs en consola
 // Rutas
 app.use("/api/products", products);
 app.use("/api/users", users);
+app.use("/api/cart", cart);
+app.use("/api/favorites", favorites);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
